e94cb2032f0fcc670f144d9ed0bfb0ffb20e3bfa
Mikita <mikita.maliauka@gmail.com>
2021-08-23 16:41:04 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -2,17 +2,32 @@ $(document).ready(function () {
     var currentFloor = 2; // текущий этаж
     var counterUp = $(".counter-up"); // увеличение этажа
     var counterDown = $(".counter-down"); // уменьшение этажа
+
     var floorPath =$(".home-image path"); // каждый отдельный этаж в svg
-    var modal =$(".modal");
-    var modalCloseButton =$(".modal-close-button");
-    var buttonPrimary =$(".button-primary");
+    var modal =$(".modal"); //класс в переменную
+    var modalCloseButton =$(".modal-close-button"); // крестик
+    var buttonPrimary =$(".button-primary"); // большая синяя кнопка
+/*
+    var currentFlat = 1; // переменная с текущей квартирой
+    var flatPath =$("modal-image path"); // квартиры SVG
+    var flatPathItem = $(".flat-item a"); // характеристики квартир
 
     // при наведении мышкой на этаж
     floorPath.on("mouseover", function () {
         currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
         floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
         $(".counter").text(currentFloor); // записываем значение этажа в счётчик
+        $(".modal-counter").text(currentFloor); // записываем значение этажа в счётчик
     });  
+*/
+    flatPath.on("mouseover", function () {
+        currentFloor = $(this).attr("data-flat");
+        flatPath.removeClass("currentFlat");
+        flatPathItem.removeClass("current-flat-item"); // удаляем класс характеристик квартиры
+        $(`[data-flat=${currentFlat}]`).toggleClass("current-flat"); // добавляем класс квартиры
+        $(`[data-item=${currentFlat}]`).toggleClass("current-flat-item"); // добавляем класс характеристик квартиры
+    });  
+
 
     floorPath.on("click", toggleModal); /*клик на этаж*/
     modalCloseButton.on("click", toggleModal); /*клик на крестик*/
@@ -24,7 +39,9 @@ $(document).ready(function () {
         if (currentFloor < 18){
             currentFloor++; // прибавляем 1 этаж
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping: false}); // форматируем переменную с этажом из 2 в 02
+            usCurrentFlat = currentFlat.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping: false});
             $(".counter").text(usCurrentFloor); // записываем значение в счётчик
+            $(".modal-counter").text(usCurrentFloor);
             floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
         }
@@ -34,7 +51,9 @@ $(document).ready(function () {
         if (currentFloor > 2){
             currentFloor--;
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping: false});
+            usCurrentFlat = currentFlat.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping: false});
             $(".counter").text(usCurrentFloor);
+            $(".modal-counter").text(usCurrentFloor);
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
@@ -43,4 +62,7 @@ $(document).ready(function () {
     function toggleModal(){
         modal.toggleClass("is-open");
     };
+
+
+
 });
